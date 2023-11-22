f25b68ee173f0e6d40cca1c9c5fa431f6e700996
Severin Urbanek <128130944+Aldiwein22@users.noreply.github.com>
2023-11-22 16:56:16 +0000

    Remove file merging functionality

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import os
 from striprtf.striprtf import rtf_to_text
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfFileReader
 
 def merge_files(directory, output_file):
     # Open the output file in write mode
+    
     with open(output_file, "w") as outfile:
-        
         # Loop through each directory, subdirectory, and file in the directory
         for dirpath, dirnames, filenames in os.walk(directory):
             for filename in filenames:
@@ -23,8 +23,12 @@ def merge_files(directory, output_file):
                         outfile.write(f"Chapter: {chapter_title}\n\n")
                         print(f"Writing chapter title for {filename}")
                         # Write the contents of the file to the output file
-                        outfile.write(infile.read())
-                        print(f"Writing contents of {filename}")
+                        contents = infile.read()
+                        if contents:
+                            outfile.write(contents)
+                            print(f"Writing contents of {filename}")
+                        else:
+                            print(f"No contents in {filename}")
                         # Add a new line after each file
                         outfile.write("\n\n")
                         print(f"Adding new line after {filename}")
@@ -39,38 +43,42 @@ def merge_files(directory, output_file):
                         outfile.write(f"Chapter: {chapter_title}\n\n")
                         print(f"Writing chapter title for {filename}")
                         # Write the plain text contents to the output file
-                        outfile.write(plain_text)
-                        print(f"Writing contents of {filename}")
+                        if plain_text:
+                            outfile.write(plain_text)
+                            print(f"Writing contents of {filename}")
+                        else:
+                            print(f"No contents in {filename}")
                         # Add a new line after each file
                         outfile.write("\n\n")
                         print(f"Adding new line after {filename}")
                 elif filename.endswith(".pdf"):
                     # Open the PDF file in read mode
-                    with open(full_path, "rb") as infile:
-                        # Read the PDF file
-                        pdf = PdfReader(infile)
-                        # Write the file name as a chapter title
-                        chapter_title = filename.replace(".pdf", "")
-                        outfile.write(f"Chapter: {chapter_title}\n\n")
-                        print(f"Writing chapter title for {filename}")
-                        # Loop through each page in the PDF
-                        for page_num in range(len(pdf.pages)):
-                            # Extract the text from the page
-                            page = pdf.pages[page_num]
-                            text = page.extract_text()
-                            # Write the page contents to the output file
+                    pdf = PdfFileReader(full_path)
+                    # Write the file name as a chapter title
+                    chapter_title = filename.replace(".pdf", "")
+                    outfile.write(f"Chapter: {chapter_title}\n\n")
+                    print(f"Writing chapter title for {filename}")
+                    # Loop through each page in the PDF
+                    for page_num in range(len(pdf.pages)):
+                        # Extract the text from the page
+                        page = pdf.pages[page_num]
+                        text = page.extract_text()
+                        # Write the page contents to the output file
+                        if text:
                             outfile.write(text)
                             print(f"Writing contents of page {page_num + 1} in {filename}")
-                            # Add a new line after each page
-                            outfile.write("\n\n")
-                            print(f"Adding new line after page {page_num + 1} in {filename}")
+                        else:
+                            print(f"No contents in page {page_num + 1} in {filename}")
+                        # Add a new line after each page
+                        outfile.write("\n\n")
+                        print(f"Adding new line after page {page_num + 1} in {filename}")
 
 def main():
     # Define the directory containing the text files and PDF files
-    directory = os.getcwd()
+    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "files")
 
     # Define the output file name
-    output_file = "merged.txt"
+    output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "merged.txt")
 
     merge_files(directory, output_file)
 
